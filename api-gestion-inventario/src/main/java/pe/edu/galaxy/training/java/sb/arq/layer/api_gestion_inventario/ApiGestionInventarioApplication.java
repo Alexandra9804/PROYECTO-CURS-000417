c7a4e2ba99c555bf2ccb0a711cd98935862fa719
Alexandra <alexandra.chavez.gallardo@outlook.es>
2025-09-28 11:53:55 -0500

    init: proyecto base de gestion de inventarios

---
+++ api-gestion-inventario/src/main/java/pe/edu/galaxy/training/java/sb/arq/layer/api_gestion_inventario/ApiGestionInventarioApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.galaxy.training.java.sb.arq.layer.api_gestion_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionInventarioApplication.class, args);
+	}
+
+}
---
+++ api-gestion-inventario/src/test/java/pe/edu/galaxy/training/java/sb/arq/layer/api_gestion_inventario/ApiGestionInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.galaxy.training.java.sb.arq.layer.api_gestion_inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
